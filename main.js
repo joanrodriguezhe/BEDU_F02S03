66b2494028a112a5792874bccaa57ad58bf6dcda
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-13 19:29:29 -0700

    Ejemplo 02

--- main.js
+++ main.js
@@ -24,3 +24,19 @@ function power(base, exponent) {
 
 console.log(power(5, 3));
 
+//Ejemplo 2
+
+const whatDoYouDo = function (job, name) {
+    switch (job) {
+        case 'developer':
+            return `${name} develops cool apps`;
+        case 'designer':
+            return `${name} designs awesome websites`;
+        default:
+            return `${name} does something else`;
+    }
+}
+
+console.log(whatDoYouDo('developer','John Doe'));
+console.log(whatDoYouDo('designer','Jane Doe'));
+console.log(whatDoYouDo('retired','Mark Doe'));
